c929e4e1e3ee050dd86a4ef7e3e84fdc87d15149
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2020-10-20 12:42:08 +0530

    displayed video

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const app = express();
 const server = require('http').Server(app);
 const {v4: uuidv4} = require('uuid');
 app.set('view engine','ejs');
+app.use(express.static('public'));
 app.get('/',(req,res)=>{
     res.redirect(`/${uuidv4()}`);
 })
-
+ 
 app.get('/:room',(req,res)=>{
     res.render('room',{roomId:req.params.room })
 }) 
