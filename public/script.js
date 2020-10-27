3e6fe95c52fafc2cfe8c7a20a22ff581ab71ba1f
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2020-10-28 00:41:50 +0530

    error in getting new users video

--- public/script.js
+++ public/script.js
@@ -2,29 +2,68 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid');
 const myVideo = document.createElement('video');
 myVideo.muted = true;
-
+var peer = new Peer(undefined,{
+    path:'/peerjs',
+    host:'/',
+    port:'3030'
+}); 
 
 let myVideoStream
 navigator.mediaDevices.getUserMedia({
     video:true,
     audio:true
 }).then(stream =>{
-myVideoStream= stream;
+myVideoStream= stream; 
 addVideoStream(myVideo,stream);
-})
-socket.emit('join-room',ROOM_ID);
-socket.on('uesr-connected',() =>{
-    connectToNewUser();
+
+peer.on('call', call =>{
+    call.answer(stream);
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream =>{
+        console.log("test case")
+        addVideoStream(video,userVideoStream)
+    })
+
 })
 
-const connectToNewUser =() =>{
-    console.log('new user');
+socket.on('uesr-connected',(userId) =>{
+    connectToNewUser(userId, stream);
+})
+})
+peer.on('open', id =>{
+    console.log(id);
+    socket.emit('join-room',ROOM_ID,id);
+})
+
+socket.emit('join-room',ROOM_ID);
+ 
+
+
+const connectToNewUser =(userId,stream) =>{
+
+
+
+
+    // console.log(userId);
+    const call = peer.call(userId,stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream =>{
+        addVideoStream(video, userVideoStream)
+    })
 }
+
+// const addVideoStream =(video, stream) =>{
+//     video.srcObject = stream
+//     video.addEventListener('loadedmetadata', () =>{
+//         video.play()
+//     })
+//     videoGrid.append(video)
+// }
 
 const addVideoStream = (video, stream) =>{
    video.srcObject = stream;
    video.addEventListener('loadedmetadata',() =>{
        video.play();
    })
-   videoGrid.append(video);
+   videoGrid.append(video); 
 }
--- server.js
+++ server.js
@@ -6,8 +6,14 @@ const server = require('http').Server(app);
 // var io = require('socket.io').listen(server);
 const io = require('socket.io')(server)
 const {v4: uuidv4} = require('uuid');
+const {ExpressPeerServer} = require('peer');
+const peerServer = ExpressPeerServer(server,{
+    debug:true
+});
+
 app.set('view engine','ejs');
 app.use(express.static('public'));
+app.use('/peerjs',peerServer);
 app.get('/',(req,res)=>{
     res.redirect(`/${uuidv4()}`);
 })
@@ -22,9 +28,9 @@ app.get('/',(req,res) =>{
 })
 
 io.on('connection', socket =>{
-    socket.on('join-room',(roomId) =>{
+    socket.on('join-room',(roomId,userId) =>{
         socket.join(roomId); 
-        socket.to(roomId).broadcast.emit('uesr-connected');
+        socket.to(roomId).broadcast.emit('uesr-connected',userId );
         console.log("joined-room")
     })
 
